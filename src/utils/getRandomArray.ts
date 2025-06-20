2e328005b5254c1de76be1b83621fd30bdaf5a83
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-06-20 13:19:39 -0300

    modified randomArray for to return integers

--- src/utils/getRandomArray.ts
+++ src/utils/getRandomArray.ts
@@ -1,3 +1,3 @@
 export function getRandomArray(size: number): number[] {
-  return Array.from({ length: size }, () => Math.random() * 100 + 4)
+  return Array.from({ length: size }, () => Math.floor(Math.random() * 100 + 4))
 }
