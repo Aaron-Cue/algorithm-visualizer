583784e5362d55e8f9e6d353aad5ac649549da4f
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-06-20 21:43:56 -0300

    added info section in layout

--- src/components/Algorithms/AlgorithmWrapperPage.tsx
+++ src/components/Algorithms/AlgorithmWrapperPage.tsx
@@ -11,9 +11,16 @@ export default function AlgorithmWrapperPage({
 }) {
   return (
     <>
-      <h1 className='text-4xl font-bold italic mb-12'>{title}</h1>
-      <section className='my-12 p-3 pb-0 pt-4 border-1 max-w-4xl mx-auto bg-slate-200'>{children}</section>
-      <div>{info}</div>
+      <h1 className="text-4xl font-bold italic mb-12">{title}</h1>
+      <section className="my-12 p-3 pb-0 pt-4 border-1 max-w-4xl mx-auto bg-slate-200">
+        {children}
+      </section>
+      <section>
+        <h2 className="text-[27px] text-slate-700 font-bold my-3 pt-4">
+          About algorithm
+        </h2>
+        <div className='px-2'>{info}</div>
+      </section>
     </>
   )
 }
