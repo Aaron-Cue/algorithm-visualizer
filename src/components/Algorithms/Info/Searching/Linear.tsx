61b630eff4e711f9dc2f5559097cdb21a00d8cc7
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-06-22 19:55:47 -0300

    Linear search info

--- src/components/Algorithms/Info/Searching/Linear.tsx
+++ src/components/Algorithms/Info/Searching/Linear.tsx
@@ -1,3 +1,25 @@
+import InfoWrapper from "../InfoWrapper"
+
 export default function Linear() {
-  return <div>Linear info</div>
+  const pseudocode = `linearSearch(arr, target):
+  for i from 0 to len(arr) - 1:
+    if arr[i] == target:
+      return i
+  return -1`
+
+  return (
+    <InfoWrapper
+      description="Linear Search is the simplest method to find an element in a list. It sequentially checks each element until the target is found or the end of the list is reached."
+      steps={[
+        'Start from the first element of the array.',
+        'Compare the current element with the target value.',
+        'If they match, return the index.',
+        'If not, move to the next element.',
+        'Repeat until the target is found or the end of the array is reached.'
+      ]}
+      pseudocode={pseudocode}
+      complexity={['O(n)', 'O(1)']}
+    />
+  )
+
 }
